d2f388889ec4db42161b0609c52ecd2dd5045441
Nikos Katsilidis <8517253-NikosKats@users.noreply.gitlab.com>
2022-10-06 19:19:33 +0300

    Final Commit

---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      serachField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.cypress.io/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ serachField: event.target.value });
+  };
+
+  render() {
+    const { robots, serachField } = this.state;
+
+    const filterRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(serachField.toLowerCase());
+    });
+
+    return !robots.length ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filterRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
