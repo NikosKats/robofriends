f9fe55b9f4b82cdf4d6cf7035c64d4171746052e
Nikos Katsilidis <8517253-NikosKats@users.noreply.gitlab.com>
2022-10-07 16:16:34 +0300

    Updates

---
+++ precache-manifest.6e474903f1d65976ac416cabb865fd1a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ba5637a7973587366fb",
+    "url": "/robofriends/static/css/main.3bbc12ef.chunk.css"
+  },
+  {
+    "revision": "3ba5637a7973587366fb",
+    "url": "/robofriends/static/js/main.3ba5637a.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "61f0ea6d82943f7f3882",
+    "url": "/robofriends/static/css/2.d5ed6111.chunk.css"
+  },
+  {
+    "revision": "61f0ea6d82943f7f3882",
+    "url": "/robofriends/static/js/2.61f0ea6d.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "65c737038a78fc42ece210357b2bc66d",
+    "url": "/robofriends/index.html"
+  }
+];
